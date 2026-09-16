994cb6cd902c5dba4706fa00ab87cb12ef9ad674
AxvenLabs <dogacan432@gmail.com>
2026-09-17 01:42:56 +0300

    REGTEST-DEMO-020: test complete E2E tamper matrix

--- tests/test_validate_regtest_e2e_evidence.py
+++ tests/test_validate_regtest_e2e_evidence.py
@@ -1,56 +1,27 @@
-import copy
-import unittest
-
+import copy,unittest
+from scripts.regtest_chain_evidence import build_chain_evidence
+from scripts.regtest_mldsa_composition import build_composition
+from scripts.regtest_benchmark_evidence import build_benchmark_evidence
+from scripts.regtest_provenance_evidence import build_provenance,EXPECTED_COMMIT,EXPECTED_TAG,EXPECTED_TAG_OBJECT
 from scripts.regtest_e2e_evidence import build_e2e_evidence
 from scripts.validate_regtest_e2e_evidence import validate_e2e_evidence
-
-
-class TestValidateRegtestE2EEvidence(unittest.TestCase):
-    def setUp(self):
-        common = {"research_only": True, "endorsed_by_bitcoin_core": False,
-                  "mainnet_intended": False, "bitcoin_core_modified": False,
-                  "parameter_set_selected": False}
-        chain = {**common, "chain_evidence_sha256": "1" * 64,
-                 "outpoint": {"txid": "a" * 64, "vout": 0},
-                 "confirmation": {"block_hash": "b" * 64, "block_height": 102},
-                 "transaction": {"raw_tx_sha256": "c" * 64, "serialized_bytes": 100,
-                                 "weight": 400, "vbytes": 100}}
-        composition = {**common, "composition_sha256": "2" * 64,
-                       "outpoint": {"txid": "a" * 64, "vout": 0},
-                       "hybrid_research_semantics": "classical-and-pq"}
-        benchmark = {**common, "benchmark_evidence_sha256": "3" * 64,
-                     "correctness_oracle_separate": True,
-                     "native_windows_resource_portability_demonstrated": False,
-                     "candidate_order": ["ML-DSA-44", "ML-DSA-65", "ML-DSA-87"]}
-        self.report = build_e2e_evidence(chain, composition, benchmark)
-
-    def test_accepts_intact_report(self):
-        self.assertEqual(validate_e2e_evidence(self.report), self.report["e2e_evidence_sha256"])
-
-    def test_rejects_digest_tamper(self):
-        altered = copy.deepcopy(self.report)
-        altered["confirmation"]["block_height"] += 1
-        with self.assertRaises(ValueError):
-            validate_e2e_evidence(altered)
-
-    def test_rejects_parameter_selection_overclaim(self):
-        altered = copy.deepcopy(self.report)
-        altered["parameter_set_selected"] = True
-        with self.assertRaises(ValueError):
-            validate_e2e_evidence(altered)
-
-    def test_rejects_candidate_reordering(self):
-        altered = copy.deepcopy(self.report)
-        altered["candidate_order"] = list(reversed(altered["candidate_order"]))
-        with self.assertRaises(ValueError):
-            validate_e2e_evidence(altered)
-
-    def test_rejects_windows_portability_overclaim(self):
-        altered = copy.deepcopy(self.report)
-        altered["native_windows_resource_portability_demonstrated"] = True
-        with self.assertRaises(ValueError):
-            validate_e2e_evidence(altered)
-
-
-if __name__ == "__main__":
-    unittest.main()
+class T(unittest.TestCase):
+ @classmethod
+ def setUpClass(cls):
+  chain=build_chain_evidence(txid="a"*64,vout=0,amount_btc="1.0",confirmations=1,block_hash="b"*64,block_height=102,tx_hex="00",tx_size=1,tx_weight=4,tx_vsize=1)
+  comp=build_composition(txid="a"*64,vout=0,message_digest="c"*64); bench=build_benchmark_evidence(3)
+  prov=build_provenance(repo_commit="d"*40,bitcoin_version="Bitcoin Core daemon version v31.1 bitcoind",source_commit=EXPECTED_COMMIT,tag=EXPECTED_TAG,tag_object=EXPECTED_TAG_OBJECT,bitcoind_sha256="e"*64,bitcoin_cli_sha256="f"*64,python_version="3.12.3",cryptography_version="48.0.0",openssl_version="OpenSSL test",os_name="Linux",machine="x86_64")
+  cls.e=build_e2e_evidence(chain,comp,bench,prov)
+ def test_ok(self): self.assertEqual(validate_e2e_evidence(self.e),self.e["e2e_evidence_sha256"])
+ def reject(self,path,value):
+  x=copy.deepcopy(self.e); cur=x
+  for p in path[:-1]: cur=cur[p]
+  cur[path[-1]]=value
+  with self.assertRaises(ValueError): validate_e2e_evidence(x)
+ def test_vout_tamper(self): self.reject(["outpoint","vout"],1)
+ def test_chain_tamper(self): self.reject(["chain_evidence","confirmation","block_height"],103)
+ def test_benchmark_tamper(self): self.reject(["benchmark_evidence","benchmark_report","candidates",0,"signature_bytes"],1)
+ def test_candidate_tamper(self): self.reject(["benchmark_evidence","benchmark_report","cross_candidate_negative","all_rejected"],False)
+ def test_provenance_tamper(self): self.reject(["provenance_evidence","openssl_version"],"tampered")
+ def test_overclaim(self): self.reject(["bitcoin_core_modified"],True)
+if __name__=="__main__": unittest.main()
